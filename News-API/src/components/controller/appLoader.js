69504d89caf25de2a353d9b8bab93a28b7000f14
Mike Prybytkin <prybytkin@bk.ru>
2022-06-29 01:06:25 +0300

    feat: add apiKey and install packages (npm i)

--- News-API/src/components/controller/appLoader.js
+++ News-API/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b6b262ef6eb24c6595a4197633967d94', // получите свой ключ https://newsapi.org/
         });
     }
 }
