fb2d332f9135dfc5aee6cfd0cf62b041b605c015
Mike Prybytkin <prybytkin@bk.ru>
2022-07-05 03:49:38 +0300

    refactor: chance API link

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'b6b262ef6eb24c6595a4197633967d94', // получите свой ключ https://newsapi.org/
         });
     }
