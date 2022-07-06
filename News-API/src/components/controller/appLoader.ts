c442c41e444afe23750542632bb8baecdf18e132
Mike Prybytkin <prybytkin@bk.ru>
2022-07-07 02:06:57 +0300

    fix: fix adaptive layout

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -4,6 +4,7 @@ class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
             // link for deploy -> nodenews.herokuapp.com
+            // link for work with code -> newsapi.org
             apiKey: 'b6b262ef6eb24c6595a4197633967d94', // получите свой ключ https://newsapi.org/
         });
     }
