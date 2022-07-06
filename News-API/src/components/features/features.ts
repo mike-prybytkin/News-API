4a9dadc506882c83fa5b24c3a328c738553578d2
Mike Prybytkin <prybytkin@bk.ru>
2022-07-07 01:50:09 +0300

    fix: change scroll padding

--- News-API/src/components/features/features.ts
+++ News-API/src/components/features/features.ts
@@ -1,11 +1,7 @@
 const buttonsContainer = document.querySelector('.buttons') as HTMLTemplateElement;
 const sourceItem = document.querySelectorAll('.source__item');
 
-function tagCurrentNews(): void {
-    buttonsContainer.addEventListener('click', (event) => {
-        sourceItem.forEach((el) => el.classList.remove('active'));
-        (event.target as HTMLInputElement).classList.add('active');
-    });
-}
-
-export default tagCurrentNews;
+buttonsContainer.addEventListener('click', (event) => {
+    sourceItem.forEach((el) => el.classList.remove('active'));
+    (event.target as HTMLInputElement).classList.add('active');
+});
