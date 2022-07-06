01024042f7ece42affa2e562ec953fbe0da11fc9
Mike Prybytkin <prybytkin@bk.ru>
2022-07-07 02:08:26 +0300

    refactor: change API link for deploy

--- News-API/src/components/controller/appLoader.ts
+++ News-API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/v2/', {
             // link for deploy -> nodenews.herokuapp.com
             // link for work with code -> newsapi.org
             apiKey: 'b6b262ef6eb24c6595a4197633967d94', // получите свой ключ https://newsapi.org/
